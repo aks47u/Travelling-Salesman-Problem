96250fc5098d613c2403f540c0eb60982eb6bb28
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-05-14 12:32:21 +0100

    Create TourManager.java

---
+++ TourManager.java
@@ -0,0 +1,24 @@
+package TSP_GA;
+
+import java.util.ArrayList;
+
+// Holds the cities of a tour
+public class TourManager {
+	// Holds our cities
+	private static ArrayList<City> destinationCities = new ArrayList<City>();
+
+	// Adds a destination city
+	public static void addCity(City city) {
+		destinationCities.add(city);
+	}
+
+	// Get a city
+	public static City getCity(int index) {
+		return (City) destinationCities.get(index);
+	}
+
+	// Get the number of destination cities
+	public static int numberOfCities() {
+		return destinationCities.size();
+	}
+}
